574e7de6394727219fb33d16a52ca7b39c348921
navdeep <navdeepsg04@gmail.com>
2025-09-28 12:54:43 -0400

    added a list of service csv, and a function to read it

--- scr/main.py
+++ scr/main.py
@@ -2,15 +2,15 @@
 import csv
 import tkinter as tk
 from tkinter import ttk
-import sv_ttk
+# import sv_ttk
 
 FONT = ("Arial", 12)
 PADDING_X, PADDING_Y = 12, 12
 
 
 def read_client_file() -> dict[str, list[str]]:
-    """Return a string dictionary with the name of the client as the key,
-    and a list of their information as the value, read from the client_list.txt.
+    """Return a dictionary with the name of the client as the key,
+    and a list of their information as the value, read from the client_list.csv.
     If there is no client list, creates an empty file instead."""
     try:
         with open("client_list.csv", 'r', newline='') as file:
@@ -21,6 +21,23 @@ def read_client_file() -> dict[str, list[str]]:
         return clients_data
     except FileNotFoundError:
         with open("client_list.csv", 'w', newline='') as _:
+            pass
+        return {}
+
+
+def read_service_file() -> dict[str, int]:
+    """Return a dictionary with the name of the service as the key,
+    and its price as the value, read from the service_list.csv.
+    If there is no service list, creates an empty file instead."""
+    try:
+        with open("service_list.csv", 'r', newline='') as file:
+            service_data = {}
+            reader = csv.reader(file)
+            for row in reader:
+                service_data[row[0]] = int(row[1])
+        return service_data
+    except FileNotFoundError:
+        with open("service_list.csv", 'w', newline='') as _:
             pass
         return {}
 
@@ -51,11 +68,11 @@ def build_interface():
             entries[labels[i]] = ttk.Entry(master=info_frm, font=FONT, state="readonly")
         entries[labels[i]].grid(row=i, column=1, sticky='we', padx=(0, PADDING_X), pady=PADDING_Y)
 
-    add_client_btn = tk.Button(master=button_frm, text="Add Client", font=FONT, width=15, height=5)
-    add_client_btn.grid(row=0, column=0, padx=PADDING_X, pady=PADDING_Y)
+    edit_client_btn = tk.Button(master=button_frm, text="Edit Client", font=FONT, width=15, height=5)
+    edit_client_btn.grid(row=0, column=0, padx=PADDING_X, pady=PADDING_Y)
 
-    add_service_btn = tk.Button(master=button_frm, text="Add Service", font=FONT, width=15, height=5)
-    add_service_btn.grid(row=1, column=0, padx=PADDING_X, pady=PADDING_Y)
+    edit_service_btn = tk.Button(master=button_frm, text="Edit Service", font=FONT, width=15, height=5)
+    edit_service_btn.grid(row=1, column=0, padx=PADDING_X, pady=PADDING_Y)
 
     # TODO replace placeholder label
     service_lbl = ttk.Label(master=service_frm, text="Placeholder")
@@ -67,4 +84,5 @@ def build_interface():
 
 if __name__ == "__main__":
     clients = read_client_file()
+    services = read_service_file()
     build_interface()
