6b4f406e843cff092bc83db33b995a436dffb4ae
navdeep <navdeepsg04@gmail.com>
2025-10-29 15:26:31 -0400

    implemented the service table and corresponding functions for adding and removing to the table

--- scr/main.py
+++ scr/main.py
@@ -117,10 +117,23 @@ class MainPage(tk.Frame):
 
         service_lbl = ttk.Label(master=service_frm, text="Service: ", font=FONT)
         service_lbl.grid(row=0, column=0, sticky="w", padx=(PADDING_X, 0), pady=PADDING_Y)
-        service_popup_entry = PopupEntry(master=service_frm, font=FONT, width=40, values=list(services.keys()))
-        service_popup_entry.grid(row=0, column=1, sticky="we", padx=(0, PADDING_X), pady=PADDING_Y)
-        service_btn = tk.Button(master=service_frm, text="Add Service", font=FONT, bg="#e8e8e8")
+        self.service_popup_entry = PopupEntry(master=service_frm, font=FONT, width=40, values=list(services.keys()))
+        self.service_popup_entry.grid(row=0, column=1, sticky="we", padx=(0, PADDING_X), pady=PADDING_Y)
+        service_btn = tk.Button(master=service_frm, text="Add Service", font=FONT,
+                                bg="#e8e8e8", command=self.add_service)
         service_btn.grid(row=0, column=2)
+
+        self.service_table = tk.Frame(master=service_frm)
+        self.service_table.grid(row=1, column=0, columnspan=3, padx=PADDING_X, pady=PADDING_Y, sticky="ew")
+        self.service_rows = []
+        self.service_list = []  # list of lists with format [[price1, amount1], [price2, amount2], ...]
+        headers = ["Services", "Price", "Amount"]
+        for col, header in enumerate(headers):
+            tk.Label(master=self.service_table, text=header, font=FONT + ("bold",), borderwidth=1, relief=tk.SOLID,
+                     pady=3).grid(row=0, column=col, sticky="nsew")
+        self.service_table.grid_columnconfigure(0, weight=1, minsize=370)
+        self.service_table.grid_columnconfigure(1, weight=1, minsize=80)
+        self.service_table.grid_columnconfigure(2, weight=1, minsize=99)
 
     def set_info(self):
         name = self.entries["Name"].get_value()
@@ -137,6 +150,59 @@ class MainPage(tk.Frame):
         self.entries[entry].delete(0, tk.END)
         self.entries[entry].insert(0, text)
         self.entries[entry].config(state="readonly")
+
+    def price_format(self, price: float) -> str:
+        return f"${price:.2f}"
+
+    def add_service(self):
+        if self.service_popup_entry.get() in services:
+            lbl_service = tk.Label(master=self.service_table, text=self.service_popup_entry.get(), font=FONT,
+                                   borderwidth=1, relief="solid", pady=3)
+            price = services[self.service_popup_entry.get()]
+            lbl_price = tk.Label(master=self.service_table, text=self.price_format(price), font=FONT,
+                                 borderwidth=1, relief="solid", pady=3)
+            frame_button = tk.Frame(master=self.service_table, borderwidth=1, relief="solid")
+            btn_minus = tk.Button(master=frame_button, text=" – ", font=FONT)
+            btn_minus.grid(row=0, column=0, padx=5, pady=3)
+            lbl_amount = tk.Label(master=frame_button, text=1, font=FONT)
+            lbl_amount.grid(row=0, column=1, pady=3)
+            btn_plus = tk.Button(master=frame_button, text=" + ", font=FONT)
+            btn_plus.grid(row=0, column=2, padx=5, pady=3)
+
+            lbl_service.grid(row=len(self.service_rows) + 1, column=0, sticky="nsew")
+            lbl_price.grid(row=len(self.service_rows) + 1, column=1, sticky="nsew")
+            frame_button.grid(row=len(self.service_rows) + 1, column=2, sticky="nsew")
+
+            row_widgets = [lbl_service, lbl_price, lbl_amount, frame_button]
+            self.service_rows.append(row_widgets)
+
+            price_amount = [price, 1]
+            self.service_list.append(price_amount)
+
+            btn_minus.config(command=lambda pr=price_amount, rw=row_widgets: self.decrease_amount(pr, rw))
+            btn_plus.config(command=lambda pr=price_amount, rw=row_widgets: self.increase_amount(pr, rw))
+
+            self.service_popup_entry.delete(0, tk.END)
+        else:
+            print(self.service_list)
+
+    def decrease_amount(self, price_amount, row_widgets):
+        if price_amount[1] > 0:
+            price_amount[1] -= 1
+            if price_amount[1] > 0:
+                row_widgets[2].config(text=price_amount[1])
+            else:
+                self.remove_service(row_widgets)
+                self.service_list.remove(price_amount)
+
+    def increase_amount(self, price_amount, row_widgets):
+        price_amount[1] += 1
+        row_widgets[2].config(text=price_amount[1])
+
+    def remove_service(self, row_widgets):
+        for widget in row_widgets:
+            widget.grid_forget()
+            widget.destroy()
 
 
 class ClientPage(tk.Frame):
