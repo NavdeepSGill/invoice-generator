8b080f2c27304602a8c629e8ed3babca528cdbb4
navdeep <navdeepsg04@gmail.com>
2025-12-21 00:56:30 -0500

    added reference to App state

--- scr/ui/client_page.py
+++ scr/ui/client_page.py
@@ -5,4 +5,5 @@ from tkinter import ttk
 class ClientPage(tk.Frame):
     def __init__(self, parent, window):
         super().__init__(parent)
-        tk.Label(master=self, text="Client Page", font=("Arial", 20)).pack()
+        self.window = window
+        tk.Label(master=self, text="Client Page", font=("Arial", 20)).pack()  # TODO remove placeholder
